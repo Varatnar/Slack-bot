f18fc7913f0b9044b83f870ea8f9188b8f12210f
RybekU <45723292+RybekU@users.noreply.github.com>
2018-12-16 17:38:30 +0100

    Basic bot setup

--- src/main.rs
+++ src/main.rs
@@ -1,51 +1,50 @@
+#[macro_use]
 extern crate serenity;
 
-//use std::env;
+mod commands;
+
+use std::{collections::HashSet, env};
 
 use serenity::{
-    model::{channel::Message, gateway::Ready},
+    framework::StandardFramework,
+    http,
+    model::{event::ResumedEvent, gateway::Ready},
     prelude::*,
-    utils::MessageBuilder,
 };
 
 struct Handler;
 
 impl EventHandler for Handler {
-    fn message(&self, _: Context, msg: Message) {
-        if msg.content == "!nintendo" {
-            let response = MessageBuilder::new()
-                .push("Let's ")
-                .push_bold_safe("smash ")
-                .push("together ")
-                .mention(&msg.author)
-                .push(".")
-                .build();
-
-            if let Err(why) = msg.channel_id.say(&response) {
-                println!("Error sending message: {:?}", why);
-            }
-        }
+    fn ready(&self, _: Context, ready: Ready) {
+        println!("Connected as {}", ready.user.name);
     }
 
-    fn ready(&self, _: Context, ready: Ready) {
-        println!("{} is connected!", ready.user.name);
+    fn resume(&self, _: Context, _: ResumedEvent) {
+        println!("Resumed");
     }
 }
 
 fn main() {
-    // Configure the client with your Discord bot token in the environment.
-    // TODO: change it to load from specific uncomitted file
-    let token = "nothing".to_owned();
-
-    // Create a new instance of the Client, logging in as a bot. This will
-    // automatically prepend your bot token with "Bot ", which is a requirement
-    // by Discord for bot users.
+    let token = env::var("DISCORD_TOKEN").expect("Expected a token in the environment");
     let mut client = Client::new(&token, Handler).expect("Err creating client");
 
-    // Finally, start a single shard, and start listening to events.
-    //
-    // Shards will automatically attempt to reconnect, and will perform
-    // exponential backoff until it reconnects.
+    let owners = match http::get_current_application_info() {
+        Ok(info) => {
+            let mut set = HashSet::new();
+            set.insert(info.owner.id);
+
+            set
+        }
+        Err(why) => panic!("Couldn't get application info: {:?}", why),
+    };
+
+    client.with_framework(
+        StandardFramework::new()
+            .configure(|c| c.owners(owners).prefix("sg "))
+            .command("ralfu", |c| c.cmd(commands::secret::ralfu::ralfu))
+            //    .command("multiply", |c| c.cmd(commands::math::multiply))
+            .command("quit", |c| c.cmd(commands::owner::quit).owners_only(true)),
+    );
     if let Err(why) = client.start() {
         println!("Client error: {:?}", why);
     }
